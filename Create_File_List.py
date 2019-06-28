23be7b09b08862ccdc8eea720cbc285a6b74a89d
Victoria Catlett <victorializcat@gmail.com>
2019-06-28 14:03:15 -0500

    Initial commit 6/28/19

---
+++ Create_File_List.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 13 14:22:35 2019
+
+@author: victoriacatlett
+"""
+
+import numpy as np
+import os.path
+import os
+
+pathname = '/Users/victoriacatlett/Documents/REU/Data/'
+
+K_names = []
+H_names = []
+
+for file in os.listdir(pathname):
+    if file.endswith('K.fits'):
+        K_names.append(file)
+    elif file.endswith('H.fits'):
+        H_names.append(file)
+
+K_path = pathname + 'K_star_names.npy'
+H_path = pathname + 'H_star_names.npy'
+        
+np.save(K_path, K_names)
+np.save(H_path, H_names)
+
